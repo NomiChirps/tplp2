e57f9754b2a4988599135b67da9397444a79f7e5
NomiChirps <nomi@goodkitty.club>
2022-07-28 09:48:32 -0700

    move hooks out of main

--- tplp/main.cc
+++ tplp/main.cc
@@ -17,30 +17,6 @@
 
 using std::chrono_literals::operator""ms;
 
-extern "C" {
-// FreeRTOS assertion failure handler
-void FreeRTOS_AssertionFailed(const char *const file, unsigned long line) {
-  panic("Assertion failed at: %s:%lu\n", file, line);
-}
-
-void FreeRTOS_ConfigureTimeForRunTimeStats() {
-  // nothing to do; pico bootloader already starts the relevant timers.
-}
-
-unsigned long FreeRTOS_GetRunTimeCounterValue() {
-  // Divide by 16 just to get smaller numbers so they fit better visually in the
-  // summary table.
-  return to_us_since_boot(get_absolute_time()) >> 4;
-}
-
-void vApplicationStackOverflowHook(TaskHandle_t task, char *name) {
-  panic("Stack overflow in task: %s\n", name);
-}
-
-void lvgl_assertion_failed() { panic("lvgl assertion failed"); }
-
-}  // extern "C"
-
 namespace tplp {
 
 void led_task(void *) {
