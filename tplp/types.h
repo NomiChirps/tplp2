33fae4388ccec062c38caf916218a340916c7bfc
NomiChirps <nomi@goodkitty.club>
2022-08-03 10:37:18 -0700

    i forgot

--- tplp/types.h
+++ tplp/types.h
@@ -4,6 +4,8 @@
 #include "NamedType/named_type.hpp"
 #include "pico/types.h"
 
+namespace tplp {
+
 using gpio_pin_t =
     fluent::NamedType<uint, struct GpioPinTag, fluent::FunctionCallable>;
 using dma_channel_t =
@@ -15,5 +17,7 @@ using dma_irq_index_t =
 // Refers to a physical IRQ number.
 using dma_irq_number_t =
     fluent::NamedType<int, struct DmaIrqNumberTag, fluent::FunctionCallable>;
+
+}  // namespace tplp
 
 #endif  // TPLP_TYPES_H_
