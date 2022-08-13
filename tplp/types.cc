4e69e73d457df029d1194284265a065272c2c146
NomiChirps <nomi@goodkitty.club>
2022-08-13 07:46:07 -0700

    i liked this format better

--- tplp/types.cc
+++ tplp/types.cc
@@ -6,7 +6,7 @@ namespace tplp {
 
 std::ostream& operator<<(std::ostream& s, const i2c_address_t& addr) {
   static_assert(sizeof(i2c_address_t::UnderlyingType) == 1);
-  return s << "I2C:0x" << std::hex << std::setw(2) << std::setfill('0')
+  return s << "0x" << std::hex << std::setw(2) << std::setfill('0')
            << (int)addr.get();
 }
 
