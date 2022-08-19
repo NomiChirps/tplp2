a3854544083fa2383cbdd9b2569afb041a246c11
NomiChirps <nomi@goodkitty.club>
2022-08-18 21:43:55 -0700

    dma_program WIP

--- tplp/bus/dma_program.cc
+++ tplp/bus/dma_program.cc
@@ -1,3 +1,282 @@
 #include "tplp/bus/dma_program.h"
 
-// nothing here yet
+#include <cstring>
+
+#include "picolog/picolog.h"
+
+namespace tplp {
+namespace {
+CompiledChain0::AliasInfo SelectAlias(const ChannelConfig& cfg) {
+  // mask() gives a bitmask of the fields that are *present*
+  // negate to get the omitted ones
+  switch ((~cfg.mask()) & 0xf) {
+    case 0x0:
+      // all fields are provided. arbitrarily choose trans_count
+      // to be the trigger
+      return {1, 3};
+    case 0x1:
+      // trans_count
+      return {1, 3};
+    case 0x2:
+      // write_addr
+      return {2, 3};
+    case 0x3:
+      // write_addr, trans_count
+      return {1, 2};
+    case 0x4:
+      // read_addr
+      return {3, 3};
+    case 0x5:
+      // read_addr, trans_count
+      return {3, 2};
+    case 0x6:
+      // read_addr, write_addr
+      return {2, 2};
+    case 0x7:
+      // read_addr, write_addr, trans_count
+      return {1, 1};
+    case 0x8:
+      // ctrl
+      return {0, 3};
+    case 0x9:
+      // ctrl, trans_count
+      return {0, 2};
+    case 0xa:
+      // ctrl, write_addr
+      // TODO: we actually could support these if we tried
+      LOG(FATAL) << "unsupported combination of omitted fields in "
+                    "ChannelConfig: ctrl, write_addr";
+    case 0xb:
+      // ctrl, write_addr, trans_count
+      return {0, 1};
+    case 0xc:
+      // ctrl, read_addr
+      LOG(FATAL) << "unsupported combination of omitted fields in "
+                    "ChannelConfig: ctrl, read_addr";
+    case 0xd:
+      // ctrl, read_addr, trans_count
+      LOG(FATAL) << "unsupported combination of omitted fields in "
+                    "ChannelConfig: ctrl, read_addr, trans_count";
+    case 0xe:
+      // ctrl, read_addr, write_addr
+      LOG(FATAL) << "unsupported combination of omitted fields in "
+                    "ChannelConfig: ctrl, read_addr, write_addr";
+    case 0xf:
+      // all fields are omitted; we can use any alias.
+      return {0, 0};
+  }
+  LOG(FATAL) << "invalid ChannelConfig::mask() value: " << cfg.mask();
+}
+
+void PackInitialConfig(const ChannelConfig& cfg,
+                       CompiledChain0::AliasInfo alias, uint8_t chain_to,
+                       ControlAliasAny* out) {
+  uint32_t ctrl = cfg.ctrl ? cfg.ctrl->Pack(true, chain_to, true) : 0;
+  switch (alias.num) {
+    case 0:
+      if (cfg.ctrl) out->al0.ctrl_trig = ctrl;
+      if (cfg.read_addr)
+        out->al0.read_addr = reinterpret_cast<intptr_t>(*cfg.read_addr);
+      if (cfg.write_addr)
+        out->al0.write_addr = reinterpret_cast<intptr_t>(*cfg.write_addr);
+      if (cfg.trans_count) out->al0.trans_count = *cfg.trans_count;
+      break;
+    case 1:
+      if (cfg.ctrl) out->al1.ctrl = ctrl;
+      if (cfg.read_addr)
+        out->al1.read_addr = reinterpret_cast<intptr_t>(*cfg.read_addr);
+      if (cfg.write_addr)
+        out->al1.write_addr = reinterpret_cast<intptr_t>(*cfg.write_addr);
+      if (cfg.trans_count) out->al1.trans_count_trig = *cfg.trans_count;
+      break;
+    case 2:
+      if (cfg.ctrl) out->al2.ctrl = ctrl;
+      if (cfg.read_addr)
+        out->al2.read_addr = reinterpret_cast<intptr_t>(*cfg.read_addr);
+      if (cfg.write_addr)
+        out->al2.write_addr_trig = reinterpret_cast<intptr_t>(*cfg.write_addr);
+      if (cfg.trans_count) out->al2.trans_count = *cfg.trans_count;
+      break;
+    case 3:
+      if (cfg.ctrl) out->al3.ctrl = ctrl;
+      if (cfg.read_addr)
+        out->al3.read_addr_trig = reinterpret_cast<intptr_t>(*cfg.read_addr);
+      if (cfg.write_addr)
+        out->al3.write_addr = reinterpret_cast<intptr_t>(*cfg.write_addr);
+      if (cfg.trans_count) out->al3.trans_count = *cfg.trans_count;
+      break;
+    default:
+      LOG(FATAL) << "internal error";
+  }
+}
+
+}  // namespace
+
+DmaProgram::DmaProgram(
+    const std::array<uint8_t, kMaxSimultaneousTransfers>& programmer_channels,
+    const std::array<uint8_t, kMaxSimultaneousTransfers>& execution_channels)
+    : programmer_channels_(programmer_channels),
+      execution_channels_(execution_channels) {}
+
+void DmaProgram::SetArg(size_t command_index,
+                        std::initializer_list<ChannelConfig> channel_configs) {
+  auto channel_config = channel_configs.begin();
+  CHECK_GE(command_index, 0u);
+  CHECK_LT(command_index, chains_.size());
+  CompiledChain1& chain = chains_[command_index];
+  int num_enabled = 0;
+  for (int t = 0; t < kMaxSimultaneousTransfers; ++t) {
+    if (!chain.cc0.enable[t]) continue;
+    ++num_enabled;
+    CHECK_NE(channel_config, channel_configs.end())
+        << "Argument list too short: expected at least " << num_enabled;
+    for (const CompiledChain1::HoleInfo& hole : chain.holes) {
+      switch (hole.type) {
+        case ChannelConfig::Field::kCtrl:
+          CHECK(channel_config->ctrl) << "ctrl must not be nullopt";
+          chain.cc0.program[hole.program_index] =
+              channel_config->ctrl->Pack(true, programmer_channels_[t], true);
+          break;
+        case ChannelConfig::Field::kReadAddr:
+          CHECK(channel_config->read_addr) << "read_addr must not be nullopt";
+          chain.cc0.program[hole.program_index] = static_cast<uint32_t>(
+              reinterpret_cast<intptr_t>(*channel_config->read_addr));
+          break;
+        case ChannelConfig::Field::kWriteAddr:
+          CHECK(channel_config->write_addr) << "write_addr must not be nullopt";
+          chain.cc0.program[hole.program_index] = static_cast<uint32_t>(
+              reinterpret_cast<intptr_t>(*channel_config->write_addr));
+          break;
+        case ChannelConfig::Field::kTransCount:
+          CHECK(channel_config->trans_count)
+              << "trans_count must not be nullopt";
+          chain.cc0.program[hole.program_index] = *channel_config->trans_count;
+          break;
+        case ChannelConfig::Field::kInvalid:
+          LOG(FATAL) << "internal error";
+      }
+    }
+    ++channel_config;
+  }
+  CHECK_EQ(channel_config, channel_configs.end())
+      << "Argument list too long: expected exactly " << num_enabled;
+}
+
+void DmaProgram::AddCommand(const DmaCommand& cmd) {
+  CompiledChain1& cc1 = chains_.emplace_back();
+  CompiledChain0& cc0 = cc1.cc0;
+  cc0.before = cmd.before;
+  cc0.after = cmd.after;
+  std::vector<uint32_t> tmp_program;
+  for (int t = 0; t < kMaxSimultaneousTransfers; ++t) {
+    cc0.enable[t] = cmd.enable[t];
+    // FIXME ? sure this is right for a disabled channel?
+    if (!cmd.enable[t]) continue;
+
+    const int num_params = cmd.transfers[t].num_params();
+    CHECK_GE(num_params, 0);
+    CHECK_LE(num_params, 4);
+    // a.k.a. programmer's transfer count
+    uint8_t control_block_length;
+    uint8_t ring_size;
+    // We can only write in power-of-2 chunks due to ring_size, so promote
+    // 3 params to 4. The fixed 4th (0th) value was added to the program above.
+    // Also if num_params is zero, we still need to write at least one word
+    // to trigger the channel.
+    switch (num_params) {
+      case 0:
+      case 1:
+        control_block_length = 1;
+        // 4 bytes of parameters
+        ring_size = 2;
+        break;
+      case 2:
+        control_block_length = 2;
+        // 8 bytes of parameters
+        ring_size = 3;
+      case 3:
+      case 4:
+        control_block_length = 4;
+        // 12 or 16 bytes of parameters
+        ring_size = 4;
+    }
+
+    cc0.alias[t] = SelectAlias(cmd.transfers[t]);
+    ControlAliasAny initial_config;
+    // Execution always chains to the programmer channel.
+    PackInitialConfig(cmd.transfers[t], cc0.alias[t], programmer_channels_[t],
+                      &initial_config);
+    // (4 - control_block_length) determines the boundary between the initial
+    // config and the runtime config; write of the initial config always starts
+    // at the 0th field of the alias.
+    cc0.initial_config_write_addr[t] = reinterpret_cast<uint32_t*>(
+        0x50000000 + 0x40 * execution_channels_[t] + 0x10 * cc0.alias[t].num);
+    cc0.initial_config_write_length[t] = 4 - control_block_length;
+    std::memcpy(cc0.initial_config[t], &initial_config, 3 * sizeof(uint32_t));
+
+    for (int index_in_chain = 0; index_in_chain < cmd.chain_length;
+         ++index_in_chain) {
+      for (int param = 0; param < num_params; ++param) {
+        cc1.holes.push_back({
+            .type = ControlAliasAny::field_type(
+                cc1.cc0.alias[t].num, cc1.cc0.alias[t].offset + param),
+            // TODO: these could be pointers too, to save
+            //       a tiny bit of arithmetic at runtime
+            .program_index = tmp_program.size(),
+        });
+        // placeholder value to be filled in at runtime
+        tmp_program.push_back(0xbaadf00d);
+      }
+      if (num_params == 0) {
+        // We need at least one non-null-trigger to run the program.
+        // Use the one arbitrarily chosen in SelectAlias, which will
+        // have been placed into the last slot of initial_config
+        // by PackInitialConfig.
+        CHECK_EQ(cc0.alias[t].offset, 3);
+        tmp_program.push_back(initial_config.field_value(cc0.alias[t].num, 3));
+      }
+      if (num_params == 3) {
+        // We need to write an extra, fixed parameter here, because ring_size
+        // for the programmer's writes can only be a power of 2. note that in
+        // this case the fixed parameter has index 0, so each cycle of the
+        // programmer writes: 1, 2, 3(trigger), [ring loops around], 0.
+        tmp_program.push_back(initial_config.field_value(cc0.alias[t].num, 0));
+      }
+    }
+    // Add a dummy control block with a null trigger to terminate the chain.
+    for (int i = 0; i < control_block_length; ++i) {
+      tmp_program.push_back(0);
+    }
+
+    // Programmer reads from the program, writes to the appropriate alias of the
+    // executor channel's control registers, and chains to the executor.
+    // See RP2040 datasheet section 2.5.7 for these offsets.
+    cc0.programmer_config[t].read_addr = 0; // filled in afterwards
+    cc0.programmer_config[t].write_addr =
+        0x50000000 + 0x40 * execution_channels_[t] + 0x10 * cc0.alias[t].num +
+        0x4 * cc0.alias[t].offset;
+    cc0.programmer_config[t].trans_count = control_block_length;
+    ChannelCtrl pctrl{
+        .high_priority = cmd.transfers[t].ctrl->high_priority,
+        .data_size = ChannelCtrl::DataSize::k32,
+        .incr_read = true,
+        .incr_write = true,
+        .ring_size = ring_size,
+        .ring_sel = 1,     // wrap writes
+        .treq_sel = 0x3f,  // unpaced
+        .bswap = false,
+        .sniff_en = false,
+    };
+    cc0.programmer_config[t].ctrl_trig =
+        pctrl.Pack(true, execution_channels_[t], true);
+  }
+  // Finalize the program so we can make pointers into it.
+  cc0.program = std::make_unique<uint32_t[]>(tmp_program.size());
+  std::copy(tmp_program.begin(), tmp_program.end(), cc0.program.get());
+  for (int t = 0; t < kMaxSimultaneousTransfers; ++t) {
+    cc0.programmer_config[t].read_addr =
+        reinterpret_cast<intptr_t>(&cc0.program[0]);
+  }
+}
+
+}  // namespace tplp
--- tplp/bus/dma_program.h
+++ tplp/bus/dma_program.h
@@ -1,6 +1,8 @@
 #ifndef TPLP_BUS_DMA_PROGRAM_H_
 #define TPLP_BUS_DMA_PROGRAM_H_
 
+#include <array>
+#include <memory>
 #include <optional>
 #include <variant>
 #include <vector>
@@ -26,92 +28,231 @@ struct Action {
 };
 
 struct ChannelCtrl {
-  // enable must be specified at compile time
-  bool high_priority;
-  uint8_t data_size;
-  bool incr_read;
-  bool incr_write;
-  uint8_t ring_size;
-  bool ring_sel;
-  // chain_to is used internally
-  uint8_t treq_sel;
-  // irq_quiet is used internally
-  bool bswap;
-  bool sniff_en;
+  enum DataSize {
+    // TODO: static assert to match thing
+    k8 = 0,
+    k16 = 1,
+    k32 = 2
+  };
+  bool high_priority = 0;
+  DataSize data_size = k8;
+  bool incr_read = 1;
+  bool incr_write = 1;
+  // Size of address wrap region. If 0, don’t wrap. For values n > 0, only the
+  // lower n bits of the address will change. This wraps the address on a
+  // (1 << n) byte boundary, facilitating access to naturally-aligned ring
+  // buffers. Valid values are 0 to 15.
+  uint8_t ring_size = 0;
+  // Select whether RING_SIZE applies to read or write addresses. If 0, read
+  // addresses are wrapped on a (1 << RING_SIZE) boundary. If 1, write addresses
+  // are wrapped.
+  bool ring_sel = 0;
+  // Set to 0x3f to disable.
+  uint8_t treq_sel = 0x3f;
+  bool bswap = false;
+  bool sniff_en = false;
+
+  static constexpr uint32_t kStaticBitsMask = 0xff207801;
+  // Packs fields into the register format, omitting enable, chain_to, and
+  // irq_quiet (which are selected by kStaticBitsMask).
+  uint32_t Pack(bool enable, uint8_t chain_to, bool irq_quiet) const {
+    return enable | (high_priority << 1) | ((data_size & 0x3) << 2) |
+           (incr_read << 4) | (incr_write << 5) | ((ring_size & 0xf) << 6) |
+           (ring_sel << 10) | ((chain_to & 0xf) << 11) |
+           ((treq_sel & 0x3f) << 15) | (irq_quiet << 21) | (bswap << 22) |
+           (sniff_en << 23);
+  }
+
+  // enable must be specified at compile time.
+  // chain_to is used internally.
+  // irq_quiet is used internally.
+  // kStaticBitsMask selects those fields in the packed representation
+  // (plus the read-only and reserved bits of the register).
 };
 
 // Configuration for one of the channels in a simultaneous transfer. All fields
 // with non-nullopt values are fixed. Any omitted fields must be supplied at
-// runtime. At least one field must be omitted.
+// runtime.
+//
+// Forbidden combinations of omitted fields (due to hardware limitations):
+//   - ctrl, write_addr
+//   - ctrl, read_addr
+//   - ctrl, read_addr, trans_count
+//   - ctrl, read_addr, write_addr
 struct ChannelConfig {
   std::optional<ChannelCtrl> ctrl;
   std::optional<volatile const void*> read_addr;
   std::optional<volatile void*> write_addr;
   std::optional<uint32_t> trans_count;
+
+  enum class Field { kInvalid, kCtrl, kReadAddr, kWriteAddr, kTransCount };
+
+  // Returns a 4-bit mask with a 1 for each field that is present.
+  // ctrl is MSB, trans_count is LSB.
+  uint8_t mask() const {
+    return trans_count.has_value() | (write_addr.has_value() << 1) |
+           (read_addr.has_value() << 2) | (ctrl.has_value() << 3);
+  }
+  // Returns the number of fields that are nullopt.
+  uint8_t num_params() const {
+    return !ctrl.has_value() + !read_addr.has_value() +
+           !write_addr.has_value() + !trans_count.has_value();
+  }
 };
 
-// A MultiTransfer can be chained with another iff:
-//   - the first has no after Action
-//   - the second one has no before Action
-//   - they have the same set of enabled channels
-//   - each enabled channel has the same set of config holes as its counterpart
 // Caution! Because channels may proceed at different rates, you cannot rely on
-// all of the transfers in one MultiTransfer completing before some of the
-// transfers in the next MultiTransfer begin. If you require this sequencing
-// guarantee, insert an Action between them.
-struct MultiTransfer {
-  bool enable[kMaxSimultaneousTransfers];
-  ChannelConfig transfers[kMaxSimultaneousTransfers];
-  std::optional<Action> before;
-  std::optional<Action> after;
+// them being in lockstep throughout a chain of transfers. If you require such a
+// sequencing guarantee, use two DmaCommands.
+struct DmaCommand {
+  bool enable[kMaxSimultaneousTransfers] = {0};
+  ChannelConfig transfers[kMaxSimultaneousTransfers] = {};
+  std::optional<Action> before = std::nullopt;
+  std::optional<Action> after = std::nullopt;
+  // TODO: document
+  int chain_length = 0;
 };
 
 // user-facing stuff above
 // compiler stuff below
 
 // The control block aliases provide many possible combinations of config
-// updates. Actually, it's all possible combinations! Wow.
+// updates.
 struct ControlAlias0 {
-  volatile const void* read_addr;
-  volatile void* write_addr;
+  uint32_t read_addr;
+  uint32_t write_addr;
   uint32_t trans_count;
+  // XXX: make sure this case is handled correctly
   uint32_t ctrl_trig;
+
+  static ChannelConfig::Field field_type(int index) {
+    switch (index) {
+      case 0:
+        return ChannelConfig::Field::kReadAddr;
+      case 1:
+        return ChannelConfig::Field::kWriteAddr;
+      case 2:
+        return ChannelConfig::Field::kTransCount;
+      case 3:
+        return ChannelConfig::Field::kCtrl;
+    }
+    return ChannelConfig::Field::kInvalid;
+  }
 };
 struct ControlAlias1 {
   uint32_t ctrl;
-  volatile const void* read_addr;
-  volatile void* write_addr;
+  uint32_t read_addr;
+  uint32_t write_addr;
   uint32_t trans_count_trig;
+
+  static ChannelConfig::Field field_type(int index) {
+    switch (index) {
+      case 0:
+        return ChannelConfig::Field::kCtrl;
+      case 1:
+        return ChannelConfig::Field::kReadAddr;
+      case 2:
+        return ChannelConfig::Field::kWriteAddr;
+      case 3:
+        return ChannelConfig::Field::kTransCount;
+    }
+    return ChannelConfig::Field::kInvalid;
+  }
 };
 struct ControlAlias2 {
   uint32_t ctrl;
   uint32_t trans_count;
-  volatile const void* read_addr;
-  volatile void* write_addr_trig;
+  uint32_t read_addr;
+  uint32_t write_addr_trig;
+
+  static ChannelConfig::Field field_type(int index) {
+    switch (index) {
+      case 0:
+        return ChannelConfig::Field::kCtrl;
+      case 1:
+        return ChannelConfig::Field::kTransCount;
+      case 2:
+        return ChannelConfig::Field::kReadAddr;
+      case 3:
+        return ChannelConfig::Field::kWriteAddr;
+    }
+    return ChannelConfig::Field::kInvalid;
+  }
 };
 struct ControlAlias3 {
   uint32_t ctrl;
-  volatile void* write_addr;
+  uint32_t write_addr;
   uint32_t trans_count;
-  volatile const void* read_addr_trig;
+  uint32_t read_addr_trig;
+
+  static ChannelConfig::Field field_type(int index) {
+    switch (index) {
+      case 0:
+        return ChannelConfig::Field::kCtrl;
+      case 1:
+        return ChannelConfig::Field::kWriteAddr;
+      case 2:
+        return ChannelConfig::Field::kTransCount;
+      case 3:
+        return ChannelConfig::Field::kReadAddr;
+    }
+    return ChannelConfig::Field::kInvalid;
+  }
 };
 union ControlAliasAny {
   ControlAlias0 al0;
   ControlAlias1 al1;
   ControlAlias2 al2;
   ControlAlias3 al3;
+
+  static ChannelConfig::Field field_type(int alias, int index) {
+    switch (alias) {
+      case 0:
+        return ControlAlias0::field_type(index);
+      case 1:
+        return ControlAlias1::field_type(index);
+      case 2:
+        return ControlAlias2::field_type(index);
+      case 3:
+        return ControlAlias3::field_type(index);
+    }
+    return ChannelConfig::Field::kInvalid;
+  }
+
+  inline uint32_t field_value(int alias, int index) {
+    return *(reinterpret_cast<uint32_t*>(&al0) + index);
+  }
 };
+static_assert(sizeof(ControlAliasAny) == 16);
 
 // This structure will be used in the interrupt handler to launch a queued
 // chain. It should be efficient for that purpose. All of the hole data
 // should be filled in already.
 struct CompiledChain0 {
-  bool enabled[kMaxSimultaneousTransfers];
+  bool enable[kMaxSimultaneousTransfers];
   // Packed sequence of null-trigger-terminated chains of control blocks.
   // Each sequence may have different widths. `programmer_config` will
-  // contain pointers into this structure.
-  uint32_t* const ctrl_buf;
+  // contain pointers into this structure. Do not modify after compilation,
+  // as this will invalidate the pointers in programmer_config.
+  std::unique_ptr<uint32_t[]> program;
+  // XXX: is this the correct alias? i don't think it matters
   ControlAlias0 programmer_config[kMaxSimultaneousTransfers];
+
+  uint32_t initial_config[kMaxSimultaneousTransfers][3];
+  uint8_t
+      initial_config_write_length[kMaxSimultaneousTransfers];  // number of
+                                                               // words, 0 to 3
+  volatile uint32_t* initial_config_write_addr[kMaxSimultaneousTransfers];
+
+  // XXX: not needed after compilation?
+  struct AliasInfo {
+    // Which ControlAlias* structure? Valid values 0, 1, 2, or 3.
+    int_fast8_t num;
+    // Offset into the ControlAlias* structure where the dynamic parameters
+    // begin, i.e. those that are supplied at runtime and may be different for
+    // each transfer in the chain. Valid values 0, 1, 2, or 3.
+    int_fast8_t offset;
+  };
+  AliasInfo alias[kMaxSimultaneousTransfers];
 
   std::optional<Action> before;
   std::optional<Action> after;
@@ -123,32 +264,53 @@ struct CompiledChain0 {
 // buffer with counting semaphore, and doesn't need to be reinvented. you
 // buffoon. <3
 struct CompiledChain1 {
-  CompiledChain0 cc;
-  // Pointers into cc.ctrl_buf.
-  const std::vector<uint32_t*> holes;
-};
+  CompiledChain0 cc0;
 
-void test() {
-  uint32_t a;
-  // the interrupt handler can use this to see which channel(s) are triggered,
-  // then index into a global array that says what to  do in each case...
-  int n = __builtin_clz(a);
-}
+  struct HoleInfo {
+    ChannelConfig::Field type;
+    // Indices into cc.program
+    size_t program_index;
+  };
+  std::vector<HoleInfo> holes;
+};
 
 class DmaProgram {
  public:
-  // XXX: what's the nicest way to pass in hole data?
-  void Execute(...);
+  explicit DmaProgram(
+      const std::array<uint8_t, kMaxSimultaneousTransfers>& programmer_channels,
+      const std::array<uint8_t, kMaxSimultaneousTransfers>& execution_channels);
+
+  void AddCommand(const DmaCommand& cmd);
+
+  // command_index must be between 0 and length()-1. The input list of
+  // ChannelConfig(s) must include ONLY the enabled channels. All ChannelConfig
+  // fields that were omitted in AddCommand() MUST be provided now. The length
+  // of channel_configs must be equal to the number of enabled channels
+  // multiplied by the chain length. Configs must be provided in channel-major
+  // order, i.e. provide all of the configs for one channel, for the length of
+  // the chain, before moving on to the next channel.
+  //
+  // CHECK-fails if these requirements are not met.
+  void SetArg(size_t command_index,
+              std::initializer_list<ChannelConfig> channel_configs);
+
+  size_t length() const { return chains_.size(); }
+
+  const std::vector<CompiledChain1>& contents() const { return chains_; }
+
  private:
-  const std::vector<CompiledChain1> chains_;
+  const std::array<uint8_t, kMaxSimultaneousTransfers> programmer_channels_;
+  const std::array<uint8_t, kMaxSimultaneousTransfers> execution_channels_;
+
+  // Each DmaCommand corresponds to exactly one compiled chain.
+  std::vector<CompiledChain1> chains_;
 };
 
-class DmaProgramCompiler {
+// in a different file...
+class DmaController {
  public:
-  // Builds up a list of CompiledChain1, pushing a new one when necessary.
-  void Add(const MultiTransfer& cmd);
-
- private:
+  DmaProgram NewProgram();
+  void Execute(const DmaProgram&);
 };
 
 // a DmaProgram can be compiled into a sequence of DMA control blocks
