10d21c184c864311f731ea8ac52a0455cfc7b64c
NomiChirps <nomi@goodkitty.club>
2022-08-19 08:46:42 -0700

    make vscode completion way faster

--- tools/refresh_compile_commands_simulator.sh
+++ tools/refresh_compile_commands_simulator.sh
@@ -2,3 +2,9 @@
 
 bazel build --config=simulate //simulator/... --keep_going
 bazel run //simulator:refresh_compile_commands -- --config=simulate
+
+# workaround for hedron-compile-commands-extractor oddness
+# see https://github.com/hedronvision/bazel-compile-commands-extractor/issues/70
+tmp=$(mktemp)
+jq 'map(select(.file | (endswith(".h") | not) and (startswith("/") | not) ))' compile_commands.json >"${tmp}"
+mv "${tmp}" compile_commands.json
--- tools/refresh_compile_commands_tests.sh
+++ tools/refresh_compile_commands_tests.sh
@@ -2,3 +2,9 @@
 
 bazel build --config=test --build_tests_only //tplp/... --keep_going
 bazel run :refresh_compile_commands_tests -- --config=test
+
+# workaround for hedron-compile-commands-extractor oddness
+# see https://github.com/hedronvision/bazel-compile-commands-extractor/issues/70
+tmp=$(mktemp)
+jq 'map(select(.file | (endswith(".h") | not) and (startswith("/") | not) ))' compile_commands.json >"${tmp}"
+mv "${tmp}" compile_commands.json
