d1d7c1c3c9073274f7f6d999153d2f6ef49bfb05
Sarah Kopecky <1832290+wembly@users.noreply.github.com>
2022-08-29 11:47:03 -0700

    Added inc/dec buttons to stepper settings

--- tplp/ui/settings_steppers.cc
+++ tplp/ui/settings_steppers.cc
@@ -1,5 +1,14 @@
+#include <cstddef>
 #include "lvgl/lvgl.h"
 #include "settings_steppers.h"
+
+static void increment_event_cb(lv_event_t * e);
+static void decrement_event_cb(lv_event_t * e);
+static void focus_event_cb(lv_event_t * e);
+
+static lv_obj_t * focused_spinner = nullptr;
+lv_obj_t * inc_btn;
+lv_obj_t * dec_btn;
 
 lv_obj_t * ui_settings_steppers_create(lv_obj_t * parent)
 {
@@ -40,11 +49,13 @@ lv_obj_t * ui_settings_steppers_create(lv_obj_t * parent)
 
     lv_obj_t * motor_1_step_count = lv_spinbox_create(content);
     lv_obj_set_width(motor_1_step_count, 100);
+    lv_obj_add_event_cb(motor_1_step_count, focus_event_cb, LV_EVENT_ALL, NULL);
     lv_obj_set_grid_cell(motor_1_step_count, LV_GRID_ALIGN_STRETCH, 1, 1,
                         LV_GRID_ALIGN_CENTER, 1, 1);
 
     lv_obj_t * motor_1_speed = lv_spinbox_create(content);
     lv_obj_set_width(motor_1_speed, 100);
+    lv_obj_add_event_cb(motor_1_speed, focus_event_cb, LV_EVENT_ALL, NULL);
     lv_obj_set_grid_cell(motor_1_speed, LV_GRID_ALIGN_STRETCH, 2, 1,
                     LV_GRID_ALIGN_CENTER, 1, 1);
 
@@ -56,34 +67,103 @@ lv_obj_t * ui_settings_steppers_create(lv_obj_t * parent)
 
     lv_obj_t * motor_2_step_count = lv_spinbox_create(content);
     lv_obj_set_width(motor_2_step_count, 100);
+    lv_obj_add_event_cb(motor_2_step_count, focus_event_cb, LV_EVENT_ALL, NULL);
     lv_obj_set_grid_cell(motor_2_step_count, LV_GRID_ALIGN_STRETCH, 1, 1,
                         LV_GRID_ALIGN_CENTER, 2, 1);
 
     lv_obj_t * motor_2_speed = lv_spinbox_create(content);
     lv_obj_set_width(motor_2_speed, 100);
+    lv_obj_add_event_cb(motor_2_speed, focus_event_cb, LV_EVENT_ALL, NULL);
     lv_obj_set_grid_cell(motor_2_speed, LV_GRID_ALIGN_STRETCH, 2, 1,
                     LV_GRID_ALIGN_CENTER, 2, 1);
 
-    /* footer buttons */
-    lv_obj_t * button_bar = lv_obj_create(content);
-    lv_obj_remove_style_all(button_bar);
-    lv_obj_set_style_pad_all(button_bar, 0, 0);
-    lv_obj_set_style_pad_gap(button_bar, 10, 0);
-    lv_obj_set_layout(button_bar, LV_LAYOUT_FLEX);
-    lv_obj_set_size(button_bar, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-    lv_obj_set_flex_flow(button_bar, LV_FLEX_FLOW_ROW);
-    lv_obj_set_grid_cell(button_bar, LV_GRID_ALIGN_END, 2, 1,
+
+    /* inc/dec footer buttons */
+    lv_obj_t * setting_button_bar = lv_obj_create(content);
+    lv_obj_remove_style_all(setting_button_bar);
+    lv_obj_set_style_pad_all(setting_button_bar, 0, 0);
+    lv_obj_set_style_pad_gap(setting_button_bar, 10, 0);
+    lv_obj_set_layout(setting_button_bar, LV_LAYOUT_FLEX);
+    lv_obj_set_size(setting_button_bar, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+    lv_obj_set_flex_flow(setting_button_bar, LV_FLEX_FLOW_ROW);
+    lv_obj_set_grid_cell(setting_button_bar, LV_GRID_ALIGN_START, 0, 2,
                     LV_GRID_ALIGN_END, 3, 1);
 
-    lv_obj_t * stop_btn = lv_btn_create(button_bar);
+    inc_btn = lv_btn_create(setting_button_bar);
+    lv_obj_t * inc_btn_label = lv_img_create(inc_btn);
+    lv_img_set_src(inc_btn_label, LV_SYMBOL_PLUS);
+    lv_obj_clear_flag(inc_btn, LV_OBJ_FLAG_CLICK_FOCUSABLE);
+    lv_obj_add_event_cb(inc_btn, increment_event_cb, LV_EVENT_ALL,  NULL);
+    lv_obj_add_state(inc_btn, LV_STATE_DISABLED);
+
+    dec_btn = lv_btn_create(setting_button_bar);
+    lv_obj_t * dec_btn_label = lv_img_create(dec_btn);
+    lv_img_set_src(dec_btn_label, LV_SYMBOL_MINUS);
+    lv_obj_clear_flag(dec_btn, LV_OBJ_FLAG_CLICK_FOCUSABLE);
+    lv_obj_add_event_cb(dec_btn, decrement_event_cb, LV_EVENT_ALL, NULL);
+    lv_obj_add_state(dec_btn, LV_STATE_DISABLED);
+
+
+    /* play/stop footer buttons */
+    lv_obj_t * play_button_bar = lv_obj_create(content);
+    lv_obj_remove_style_all(play_button_bar);
+    lv_obj_set_style_pad_all(play_button_bar, 0, 0);
+    lv_obj_set_style_pad_gap(play_button_bar, 10, 0);
+    lv_obj_set_layout(play_button_bar, LV_LAYOUT_FLEX);
+    lv_obj_set_size(play_button_bar, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+    lv_obj_set_flex_flow(play_button_bar, LV_FLEX_FLOW_ROW);
+    lv_obj_set_grid_cell(play_button_bar, LV_GRID_ALIGN_END, 2, 1,
+                    LV_GRID_ALIGN_END, 3, 1);
+
+    lv_obj_t * stop_btn = lv_btn_create(play_button_bar);
     lv_obj_set_style_bg_color(stop_btn, lv_palette_main(LV_PALETTE_RED), 0);
     lv_obj_t * stop_btn_label = lv_img_create(stop_btn);
     lv_img_set_src(stop_btn_label, LV_SYMBOL_STOP);
 
-    lv_obj_t * play_btn = lv_btn_create(button_bar);
+    lv_obj_t * play_btn = lv_btn_create(play_button_bar);
     lv_obj_set_style_bg_color(play_btn, lv_palette_main(LV_PALETTE_GREEN), 0);
     lv_obj_t * play_btn_label = lv_img_create(play_btn);
     lv_img_set_src(play_btn_label, LV_SYMBOL_PLAY);
 
     return content;
+}
+
+static void focus_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * spinner = lv_event_get_target(e);
+    // lv_obj_t * kb = lv_event_get_user_data(e);
+    if(code == LV_EVENT_FOCUSED) {
+        focused_spinner = spinner;
+        lv_obj_add_state(spinner, LV_STATE_FOCUS_KEY);
+        lv_obj_clear_state(inc_btn, LV_STATE_DISABLED);
+        lv_obj_clear_state(dec_btn, LV_STATE_DISABLED);
+    }
+
+    if(code == LV_EVENT_DEFOCUSED) {
+        focused_spinner = nullptr;
+        lv_obj_clear_state(spinner, LV_STATE_FOCUS_KEY);
+        lv_obj_add_state(inc_btn, LV_STATE_DISABLED);
+        lv_obj_add_state(dec_btn, LV_STATE_DISABLED);
+    }
+}
+
+static void increment_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    if(code == LV_EVENT_SHORT_CLICKED || code  == LV_EVENT_LONG_PRESSED_REPEAT) {
+        if(focused_spinner != nullptr) {
+            lv_spinbox_increment(focused_spinner);
+        }
+    }
+}
+
+static void decrement_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    if(code == LV_EVENT_SHORT_CLICKED || code == LV_EVENT_LONG_PRESSED_REPEAT) {
+        if(focused_spinner != nullptr) {
+            lv_spinbox_decrement(focused_spinner);
+        }
+    }
 }
