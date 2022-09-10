94a7d49881c4cb675ef3a31df1485e2f5e72d93b
NomiChirps <nomi@goodkitty.club>
2022-09-10 10:50:22 -0700

    let RETURN_IF_ERROR work with StatusOr.status()

--- picolog/status_macros.h
+++ picolog/status_macros.h
@@ -8,19 +8,16 @@
 #define STATUS_MACROS_PREDICT_FALSE(x) (__builtin_expect(x, 0))
 
 namespace util {
-namespace status_macros_internal {
-using ::util::Status;
-}
 // Run a command that returns a util::Status.  If the called code returns an
 // error status, return that status up out of this method too.
 //
 // Example:
 //   RETURN_IF_ERROR(DoThings(4));
-#define RETURN_IF_ERROR(expr)                                                \
-  do {                                                                       \
-    /* Using _status below to avoid capture problems if expr is "status". */ \
-    ::util::status_macros_internal::Status _status = (expr);                 \
-    if (STATUS_MACROS_PREDICT_FALSE(!_status.ok())) return _status;          \
+#define RETURN_IF_ERROR(expr)                                                  \
+  do {                                                                         \
+    /* Using _status below to avoid capture problems if expr is "status". */   \
+    auto&& _status = (expr);                                                   \
+    if (STATUS_MACROS_PREDICT_FALSE(!_status.ok())) return std::move(_status); \
   } while (0)
 
 // Internal helper for concatenating macro values.
@@ -35,7 +32,7 @@ Status DoAssignOrReturn(T& lhs, StatusOr<T>&& result) {
   return std::move(result).status();
 }
 
-#define ASSIGN_OR_RETURN_IMPL(status, lhs, rexpr) \
+#define ASSIGN_OR_RETURN_IMPL(status, lhs, rexpr)         \
   ::util::Status status = DoAssignOrReturn(lhs, (rexpr)); \
   if (STATUS_MACROS_PREDICT_FALSE(!status.ok())) return status;
 
